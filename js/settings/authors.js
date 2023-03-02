d2e50f77190d2c6268a6a7fcc3297483bfdc186d
sungeun101 <61957322+sungeun101@users.noreply.github.com>
2023-03-02 17:41:40 +0900

    Fix daitepicker on modal

--- js/settings/authors.js
+++
@@ -1,8 +0,0 @@
-// 사용자 권한 등록 모달
-const authorRegisterShowDialogButton = document.getElementById(
-  'authorRegisterShowDialogButton',
-);
-const authorRegisterDialog = document.getElementById('authorRegisterDialog');
-authorRegisterShowDialogButton.addEventListener('click', () => {
-  authorRegisterDialog.showModal();
-});
