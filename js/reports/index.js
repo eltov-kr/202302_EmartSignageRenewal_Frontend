9ea0a3903e608b2537e8e1214f91506943eae77d
sungeun101 <61957322+sungeun101@users.noreply.github.com>
2023-03-14 09:10:45 +0900

    Update relative paths (js)

--- js/reports/index.js
+++ js/reports/index.js
@@ -7,15 +7,15 @@ const buttonName = document.querySelector('#reports-index-downloadButton span');
 graph.classList.add('hidden');
 
 const showTable = () => {
-  tableIcon.src = '/images/btn_report_list01_o.png';
-  graphIcon.src = '/images/btn_report_list02.png';
+  tableIcon.src = '../../images/btn_report_list01_o.png';
+  graphIcon.src = '../../images/btn_report_list02.png';
   table.classList.remove('hidden');
   graph.classList.add('hidden');
   buttonName.innerText = '엑셀 다운받기';
 };
 const showGraph = () => {
-  tableIcon.src = '/images/btn_report_list01.png';
-  graphIcon.src = '/images/btn_report_list02_o.png';
+  tableIcon.src = '../../images/btn_report_list01.png';
+  graphIcon.src = '../../images/btn_report_list02_o.png';
   graph.classList.remove('hidden');
   graph.classList.add('grid');
   table.classList.add('hidden');
